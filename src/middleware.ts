3c3fa32b61e83d16df90fbb75fefe10f2606e28f
Atharva0811 <atharvaus008@gmail.com>
2025-07-29 13:25:14 +0530

    cookie import

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,14 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { cookies } from "next/headers";
 
 const AUTH_COOKIE_NAME = process.env.TOKEN_NAME!;
 const CORRECT_COOKIE_VALUE = process.env.TOKEN!;
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
-  console.log(pathname);
-
+  const cookie = await cookies();
   const FORM_DYNAMIC_PATH_REGEX = /^\/form\/[^/]+$/;
-
   const UNPROTECTED_PATHS = ["/", "/form/"];
 
   if (UNPROTECTED_PATHS.includes(pathname)) {
@@ -20,7 +19,7 @@ export function middleware(request: NextRequest) {
     return NextResponse.next();
   }
 
-  const authToken = request.cookies.get(AUTH_COOKIE_NAME);
+  const authToken = cookie.get(AUTH_COOKIE_NAME);
 
   if (authToken && authToken.value === CORRECT_COOKIE_VALUE) {
     return NextResponse.next();
@@ -29,8 +28,10 @@ export function middleware(request: NextRequest) {
   console.log(
     `Middleware: Unauthorized access attempt to ${pathname}. Redirecting to ${UNPROTECTED_PATHS[0]}`
   );
-  console.log(`cookie:accessed:${CORRECT_COOKIE_VALUE} and ${authToken} and ${AUTH_COOKIE_NAME}`);
-  
+  console.log(
+    `cookie:accessed:${CORRECT_COOKIE_VALUE} and ${authToken} and ${AUTH_COOKIE_NAME}`
+  );
+
   return NextResponse.redirect(new URL(UNPROTECTED_PATHS[0], request.url));
 }
 
